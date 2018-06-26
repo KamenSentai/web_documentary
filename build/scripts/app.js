e88d8b763ea1d4afab56a559bddcb8050a187bf6
AlainKRSS <cvt.alain@gmail.com>
2018-06-26 14:03:38 +0200

    🎨 scripts > Add overlay

--- build/scripts/app.js
+++ build/scripts/app.js
@@ -1,3 +1,4 @@
 import './libs/modernizr'
 import './libs/water'
 import './components/canvas'
+import './components/overlay'
---
+++ build/scripts/components/overlay.js
@@ -0,0 +1,8 @@
+const overlay = document.createElement('div')
+overlay.style.position   = 'absolute'
+overlay.style.left       = '0'
+overlay.style.top        = '0'
+overlay.style.width      = '100%'
+overlay.style.height     = '100%'
+overlay.style.background = 'radial-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1))'
+document.body.appendChild(overlay)
