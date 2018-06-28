3cce410e41cf735545c5f52da72189f7672c622f
AlainKRSS <cvt.alain@gmail.com>
2018-06-28 14:36:24 +0200

    🚦 views > Try to fix waving sidebar

--- build/scripts/components/wave.js
+++ build/scripts/components/wave.js
@@ -1,41 +1,40 @@
 import * as dynamics from '../libs/dynamics'
 
-(function() {
-    let $path = document.querySelector('.sideBarWave path')
-    let $from = $path.getAttribute('d')
-    let $to = $path.dataset['to']
 
-    let $options = {
-        type: dynamics.easeOut,
-        durations: 1858,
-        friction: 315
+let $path = document.querySelector('.sideBarWave path')
+let $from = $path.getAttribute('d')
+let $to = $path.dataset['to']
+
+let $options = {
+    type: dynamics.easeOut,
+    durations: 1858,
+    friction: 315
 }
 
-    let sidebarOpen = false
+let sidebarOpen = false
 
-    console.log("$From : " +$from)
-    console.log("$To : " +$to)
-    console.log("Dynamics : " +dynamics)
-    console.log($path)
+console.log("$From : " +$from)
+console.log("$To : " +$to)
+console.log("Dynamics : " +dynamics)
+console.log($path)
 
-    let close = document.querySelector('.about-close-button')
-    let open = document.querySelector('.chapter-medias-button')
+let close = document.querySelector('.about-close-button')
+let open = document.querySelector('.chapter-medias-button')
 
-    open.addEventListener('click', function (e) {
-        e.stopPropagation()
-        e.preventDefault()
+open.addEventListener('click', function (e) {
+    e.stopPropagation()
+    e.preventDefault()
 
-        dynamics.animate($path, {
-            d: $to
-        }, $options)
-    })
+    dynamics.animate($path, {
+        d: $to
+    }, $options)
+})
 
-    close.addEventListener('click', function (e) {
-        e.stopPropagation()
-        e.preventDefault()
+close.addEventListener('click', function (e) {
+    e.stopPropagation()
+    e.preventDefault()
 
-        dynamics.animate($path, {
-            d: $from
-        }, $options)
-    })
-})()
+    dynamics.animate($path, {
+        d: $from
+    }, $options)
+})
